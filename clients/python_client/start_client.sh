7d6f1aac01f38c9ced0506d1323082f62b2e048c
EMCP <emcp@whichdegree.co>
2020-05-14 19:37:54 +0200

    upload helper scripts to start the server and call client

---
+++ clients/python_client/start_client.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd "$parent_path"
+
+export PYTHONPATH="$parent_path"/gen_py:$PYTHONPATH
+export PYTHONPATH="$parent_path":$PYTHONPATH
+
+echo "$PYTHONPATH"
+
+# uses system python interpreter
+python3 "$parent_path"/myclient/theclient.py
